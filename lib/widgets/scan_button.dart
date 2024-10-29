7e516338458c1bdb3b06d8f63265f4fb5d13c98c
Omar Llinares <llinares.omar@gmail.com>
2024-10-29 11:40:31 +0100

    va el scaner

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-        child: Icon(Icons.filter_center_focus), onPressed: () {});
+      child: Icon(Icons.filter_center_focus),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => QRScannerScreen(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class QRScannerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Escanear código QR')),
+      body: MobileScanner(
+        onDetect: (BarcodeCapture capture) {
+          final Barcode? barcode = capture.barcodes.first;
+          final String? code = barcode?.rawValue;
+          if (code != null) {
+            Navigator.pop(context); // Cierra el escáner
+            print('QR Code detected: $code');
+          }
+        },
+      ),
+    );
   }
 }
